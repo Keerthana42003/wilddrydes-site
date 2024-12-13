9f740faa0f3a00c55b46ed07778a234b265bbcd8
Keerthana42003 <keerthanashettyputa123@gmail.com>
2024-12-13 21:21:33 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,48 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_IGEXrzYDb', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '307ojfc7r8vsogvrjf9l4vipef', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        userPoolClientsecret: 'a26eu54jn80kah5kgm30p6262ckvdo6klvo67389uk2ucqj7nqe',
+        userPoolClientsecret: 'a26eu54jn80kah5kgm30p6262ckvdo6klvo67389uk2ucqj7nqe', // Your Cognito Client Secret
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
+// Function to generate the SECRET_HASH
+function generateSecretHash(username) {
+    const clientId = window._config.cognito.userPoolClientId;
+    const clientSecret = window._config.cognito.userPoolClientsecret;
+
+    const message = username + clientId;
+    const hmac = CryptoJS.HmacSHA256(message, clientSecret);  // Using CryptoJS for HMAC SHA256
+    const secretHash = CryptoJS.enc.Base64.stringify(hmac);
+    
+    return secretHash;
+}
+
+// Example of using the `SECRET_HASH` when calling Cognito API
+
+function initiateAuth(username, password) {
+    const secretHash = generateSecretHash(username);
+
+    const params = {
+        AuthFlow: 'USER_PASSWORD_AUTH',
+        ClientId: window._config.cognito.userPoolClientId,
+        AuthParameters: {
+            USERNAME: username,
+            PASSWORD: password,
+            SECRET_HASH: secretHash
+        }
+    };
+
+    const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+
+    cognitoidentityserviceprovider.initiateAuth(params, function(err, data) {
+        if (err) {
+            console.log('Error:', err);
+        } else {
+            console.log('Success:', data);
+        }
+    });
+}
