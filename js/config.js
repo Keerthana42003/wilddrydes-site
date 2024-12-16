b7560b161bf6a1e9574e40ba9c4d8e2483806b93
Keerthana42003 <keerthanashettyputa123@gmail.com>
2024-12-16 12:30:12 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_uEZwcb6z6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'cck0pbpjerss5n8u43ca6v4ln', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_aOWaZeWkk', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7poi1pokj965m3jqvnovfacha', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
